17e65c56463d8b8c6ae191680725ca978cd9c251
MayDan12 <oluwamayowadan@gmail.com>
2024-05-21 11:53:23 +0000

    validated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,38 +6,37 @@ UTF8 validation task
 
 def validUTF8(data):
     """
-    valid UTF*
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    num_bytes = 0
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    number_bytes = 0
 
-    # Iterate through each byte in the data
-    for byte in data:
-        # Get the 8 least significant bits of the byte
-        byte = byte & 0xFF
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-        if num_bytes == 0:
-            if (byte & mask1) == 0:
-                # 1-byte character (ASCII)
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
-            elif (byte & mask1) and (byte & mask2) == 0:
-                # Invalid if starts with 10xxxxxx
-                return False
-            else:
-                # Calculate the number of bytes
-                num_ones = 0
-                while (byte & mask1):
-                    num_ones += 1
-                    byte <<= 1
-                if num_ones == 1 or num_ones > 4:
-                    return False
-                num_bytes = num_ones - 1
-        else:
-            # Check if it is a valid continuation byte (10xxxxxx)
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-            num_bytes -= 1
 
-    # All characters should be properly completed
-    return (num_bytes == 0)
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
